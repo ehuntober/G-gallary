ef40054fce79528baf0067c96a76be69c2ba6057
ehuntober <ehuntercrypto@gmail.com>
2023-01-15 17:48:29 +0000

    model migrations

--- photo/models.py
+++ photo/models.py
@@ -8,3 +8,11 @@ class Category(models.Model):
     def __str__(self):
         return self.name
         
+
+class Photo(models.Model):
+    category= models.ForeignKey(Category,on_delete = models.SET_NULL,null=True)
+    image = models.ImageField(null=False, blank=False)
+    description= models.TextField()
+    
+    def __str__(self):
+        return self.description
