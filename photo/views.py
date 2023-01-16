ff20fbe3a1c4ee5f7bf4729ccb0e11edc74feed8
ehuntober <ehuntercrypto@gmail.com>
2023-01-16 05:28:59 +0000

    staticfiles collected

--- photo/views.py
+++ photo/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from .models import Category , Photo 
 
 # Create your views here.
 
 def gallery(request):
+    categories = Category.objects.all()
+    context = {'categories': categories}
     return render(request,'photos/gallery.html')
 
 def viewPhoto(request,pk):
